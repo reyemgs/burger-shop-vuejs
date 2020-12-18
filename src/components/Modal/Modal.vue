aaed2684a42a6825ca3a11867da3515a8b9e126e
reyemgs <thetortyan@gmail.com>
2020-12-18 16:55:52 +0300

    feat: default popup for multiple products

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
+            <slot name="header">
+              default header
+              <button class="modal-default-button" @click="$emit('close')">
+                X
+              </button>
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" src="./modal.scss"></style>
