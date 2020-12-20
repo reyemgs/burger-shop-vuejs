af2629a7e99a1f3c211346a383fbb990e24c54eb
reyemgs <shcherbinin.anton.v@gmail.com>
2020-12-20 14:30:37 +0300

    chore: modal nav menu, product in prop

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -4,25 +4,28 @@
       <div class="modal-wrapper">
         <div class="modal-container">
           <div class="modal-header">
-            <slot name="header">
-              default header
-              <button class="modal-default-button" @click="$emit('close')">
-                X
-              </button>
-            </slot>
+            <span class="modal-title">{{ modalItems[0].title }}</span>
+            <div class="modal-close-btn">
+              <i
+                class="fas fa-times"
+                aria-hidden="true"
+                @click="$emit('closeModal')"
+              ></i>
+            </div>
           </div>
 
           <div class="modal-body">
-            <slot name="body">
-              default body
-            </slot>
+            <ul class="modal-nav">
+              <ModalNavItem
+                v-for="item of modalItems"
+                :key="item.id"
+                :item="item"
+              />
+            </ul>
+            <div class="modal-content"></div>
           </div>
 
-          <div class="modal-footer">
-            <slot name="footer">
-              default footer
-            </slot>
-          </div>
+          <div class="modal-footer">&#8381; {{ product.price }}</div>
         </div>
       </div>
     </div>
@@ -30,7 +33,22 @@
 </template>
 
 <script>
-export default {};
+import ModalNavItem from './ModalNavItem.vue';
+
+export default {
+  components: {
+    ModalNavItem,
+  },
+
+  props: {
+    modalItems: {
+      type: Array,
+    },
+    product: {
+      type: Object,
+    },
+  },
+};
 </script>
 
 <style lang="scss" src="./modal.scss"></style>
---
+++ src/components/Modal/ModalNavItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="modal-nav-item">{{ item.name }}</li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style lang="scss" src="./modalNavItem.scss"></style>
