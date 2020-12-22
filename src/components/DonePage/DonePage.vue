41d27ca9a233c31376f403628bb5062c80f0ec88
reyemgs <thetortyan@gmail.com>
2020-12-22 12:21:17 +0300

    feat: done page render

--- src/components/DonePage/DonePage.vue
+++ src/components/DonePage/DonePage.vue
@@ -1,9 +1,72 @@
 <template>
-  <div></div>
+  <div class="done-page">
+    <img class="product-image" :src="product.image" />
+    <div class="done-wrapper">
+      <span class="done-header">Ваш сендвич готов!</span>
+      <div class="done-info">
+        <span class="done-size">{{ sizes }}</span>
+        <span class="done-bread">{{ breads }}</span>
+        <span class="done-vegetables">{{ vegetables }}</span>
+        <span class="done-sauces">{{ sauces }}</span>
+        <span class="done-fillings">{{ fillings }}</span>
+      </div>
+      <span class="done-name">{{ product.name }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    product: {
+      type: Object,
+    },
+    ingridients: {
+      type: Object,
+    },
+  },
+
+  data() {
+    return {
+      components: this.product.components,
+      allIngridients: this.ingridients,
+    };
+  },
+
+  computed: {
+    sizes() {
+      return this.allIngridients.sizes[this.components.sizes].name;
+    },
+
+    breads() {
+      return this.allIngridients.breads[this.components.breads].name;
+    },
+
+    vegetables() {
+      const vegetables = [];
+      for (const component of this.components.vegetables) {
+        vegetables.push(this.allIngridients.vegetables[component].name);
+      }
+      return vegetables.length !== 0 ? vegetables.join(', ') : 'Нет';
+    },
+
+    sauces() {
+      const sauces = [];
+      for (const component of this.components.sauces) {
+        sauces.push(this.allIngridients.sauces[component].name);
+      }
+      return sauces.length !== 0 ? sauces.join(', ') : 'Нет';
+    },
+
+    fillings() {
+      const fillings = [];
+      for (const component of this.components.fillings) {
+        fillings.push(this.allIngridients.fillings[component].name);
+      }
+      return fillings.length !== 0 ? fillings.join(', ') : 'Нет';
+    },
+  },
+};
 </script>
 
 <style lang="scss" src="./donePage.scss"></style>
