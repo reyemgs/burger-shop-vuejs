ee39c7419665c6432b9817cd5602bd8e879d0df8
reyemgs <thetortyan@gmail.com>
2020-12-23 17:13:19 +0300

    feat: update price, remove product, add new prop
    update price when price with ingridients change
    add new prop originalComponents using lodash cloneDeep
    change components to original when remove from cart
    change mounted method to async

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
         />
         <Basket
           :added-products="addedProducts"
+          @removeProduct="removeProduct"
           @resetQuantity="resetQuantity"
         />
       </div>
@@ -21,9 +22,9 @@
           :product="product"
           :ingridients="ingridients"
           :markets="response.markets"
-          :modalIsOpen="showModal"
           @addInBasket="addInBasket"
           @updateQuantity="updateQuantity"
+          @updatePrice="updatePrice"
           @showModal="openModal"
         />
       </div>
@@ -33,7 +34,6 @@
       :product="currentProduct"
       :ingridients="ingridients"
       :modalItems="response.modal"
-      :modalIsOpen="showModal"
       @closeModal="closeModal"
       @addInBasket="addInBasket"
       @updateQuantity="updateQuantity"
@@ -49,6 +49,7 @@ import MenuList from '@/components/MenuList/MenuList.vue';
 import Basket from '@/components/Basket/Basket.vue';
 import ProductCard from '@/components/ProductCard/ProductCard.vue';
 import Modal from '@/components/Modal/Modal.vue';
+import { cloneDeep } from 'lodash';
 
 export default {
   name: 'App',
@@ -86,9 +87,6 @@ export default {
       let products = this.response.menu;
       let id = 1;
       for (const product of products) {
-        if (product.type === 'multiple') {
-          Vue.set(product, 'customPrice', product.price);
-        }
         Vue.set(product, 'id', id++);
         Vue.set(product, 'quantity', 1);
       }
@@ -109,15 +107,23 @@ export default {
     },
   },
 
-  mounted() {
-    fetch(this.url)
-      .then(response => response.json())
+  async mounted() {
+    const response = await fetch(this.url)
+      .then(response => {
+        return response.json();
+      })
       .then(json => {
-        this.response = json;
-        this.loading = false;
-        this.renderProductsByCategory(this.currentCategory);
+        return json;
       })
       .catch(error => console.error(error));
+    this.response = {
+      ...response,
+      products: response.menu.map(product => {
+        Vue.set(product, 'originalComponents', cloneDeep(product.components));
+      }),
+    };
+    this.loading = false;
+    this.renderProductsByCategory(this.currentCategory);
   },
 
   methods: {
@@ -151,6 +157,15 @@ export default {
       }
     },
 
+    removeProduct(product) {
+      const index = this.addedProducts.findIndex(item => item === product);
+      const foundProduct = this.products.find(item => item.id === product.id);
+      this.addedProducts.splice(index, 1);
+      if (foundProduct.components) {
+        foundProduct.components = cloneDeep(foundProduct.originalComponents);
+      }
+    },
+
     resetQuantity(product) {
       const resetedProduct = this.products.find(item => item.id === product.id);
       resetedProduct.quantity = 1;
@@ -159,6 +174,16 @@ export default {
     updateModalQuantity(product) {
       const updatedProduct = this.products.find(item => item.id === product.id);
       updatedProduct.quantity = product.quantity;
+    },
+
+    updatePrice(price) {
+      this.currentProduct.price = price;
+      const foundProduct = this.addedProducts.find(
+        item => item.id === this.currentProduct.id
+      );
+      if (foundProduct) {
+        foundProduct.price = price;
+      }
     },
 
     addInBasket(product) {
